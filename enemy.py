9d0a59e8d4faa202f0b212cdd0f48c9ba096a377
AaronPolite <98177555+AaronPolite@users.noreply.github.com>
2022-02-17 20:31:28 -0500

    Added Player and Enemy with Sprites that move, as well as making movement dependant on variable dt.

--- enemy.py
+++ enemy.py
@@ -1,6 +1,7 @@
 import pygame  
+
 """finds the images form your file and allows vcs to access them""" 
-slime = pygame.image.load(r'C:\Users\Eric\Computer Science Game Project\sprite\Slime01.png').convert()
+slime = pygame.image.load("sprites/Slime01.png")
 
 class Enemy():
     def __init__(self, x, y, width, height, end, win):
@@ -13,35 +14,35 @@ class Enemy():
 
         """This sets the end points of the enemy"""
         self.path = [x, end] 
-        
+
         """How fast the images is moving"""
-        self.vel = .5
+        self.vel = 200
 
 
     """Draws the images on the screen with its x and y values"""
-    def draw(self, win):
+    def draw(self, win, dt):
         win.blit(slime, (self.x, self.y))
 
         """alllows the images to move"""
-        self.move()
-        
+        self.move(dt)
 
-    def move(self):
+
+    def move(self, dt):
         """handles the movemet of the enemy"""
         if self.vel > 0:
             """Checks if the enemy is moving past its right end point"""
             if self.x < self.path[1] + self.vel:
-                self.x += self.vel
+                self.x += self.vel * dt
             else:
                 """Changes the direction of the enemy"""
                 self.vel = self.vel * -1
-                self.x += self.vel
+                self.x += self.vel * dt
                 self.walkCount = 0
         else:
             """Checks if the enemy is reaching the left end point"""
             if self.x > self.path[0] - self.vel:
-                self.x += self.vel
+                self.x += self.vel * dt
             else:
                 self.vel = self.vel * -1
-                self.x += self.vel
-                self.walkCount = 0
+                self.x += self.vel  * dt
+                self.walkCount = 0 
